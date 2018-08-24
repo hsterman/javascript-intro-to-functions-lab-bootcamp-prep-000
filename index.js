6c4afdedce41f397eedebb4ef95ff484cdb7bf99
heather sterman <heather.sterman531@gmail.com>
2018-08-24 00:00:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   } else if (string === upperCase) {
     return "YES INDEED!"
-  } else () {
+  } else if (string === ) {
     
   }
 }
